1ef6c5c8ea75ae04db786abb4d491252c77e6306
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-02-15 08:55:32 +0545

    Update Insertion_sort_algorithms.java

---
+++ Insertion_sort_algorithms.java
@@ -0,0 +1,26 @@
+import java.io.PushbackInputStream;
+
+public class Insertion_sort_algorithms {
+    public static void Insertion_sort(int arr[]){
+        for(int i = 1; i < arr.length; i++){
+            int curr = arr[i];
+            int pre = i-1;
+            while(pre >= 0 && arr[pre] > curr){
+                arr[pre + 1] = arr[pre];
+                pre--;
+            }
+            arr[pre+1] = curr;
+        }
+         
+    }
+    public static void print(int arr[]){
+        for(int i = 0; i < arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,1,3,8,5,9,7};
+        Insertion_sort(arr);
+        print(arr);
+    }
+}
