e0e304353f5be643f13126e312c4eb78eb9e5873
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-02-21 12:35:22 +0545

    Update Quick_sort.java

---
+++ Quick_sort.java
@@ -0,0 +1,14 @@
+
+public class Quick_sort {
+    public static void Quicksort(int arr[],int si,int ei) {
+    int pivort = arr[ei];
+    Quicksort(arr, si, );
+
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,4,2,7,3,6,0};
+        Quicksort(arr, 0, arr.length-1;);
+    }
+}
